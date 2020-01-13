b0c34ae44f1f5ba44afb81db8753cf42b38082d8
Dig <Digvijay.Solanki@zapcg.com>
2020-01-13 16:52:55 +0530

    Removed Passing of App Context to Koin

--- app/src/main/java/com/dig/goodreads/App.kt
+++ app/src/main/java/com/dig/goodreads/App.kt
@@ -11,7 +11,6 @@ class App : Application() {
         super.onCreate()
         Timber.plant(Timber.DebugTree())
         startKoin {
-            androidContext(this@App)
             modules(listOf(dataSourceModule,viewModelModule))
         }
     }
